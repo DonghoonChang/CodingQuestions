33679dc1facfc49d8bc03546102201064b832221
donghoon-desktop <varensio3@gmail.com>
2022-07-08 01:20:18 +1000

    BearAndSteadyGene myImplementation

--- src/ProblemSolving/Algorithms/Strings/BearAndSteadyGene.java
+++ src/ProblemSolving/Algorithms/Strings/BearAndSteadyGene.java
@@ -34,39 +34,54 @@ public class BearAndSteadyGene {
         }
 
         public static int steadyGene(String gene) {
-            int min = Integer.MAX_VALUE;
             int length = gene.length();
             int stable = length / 4;
-            HashMap<Character, Integer> count = new HashMap();
+
+            HashMap<Character, Integer> count = new HashMap<>();
             count.put('A', 0);
             count.put('C', 0);
             count.put('G', 0);
             count.put('T', 0);
 
-            int windowL;
-            for(windowL = 0; windowL < length; ++windowL) {
-                count.put(gene.charAt(windowL), count.get(gene.charAt(windowL)) + 1);
+            // O(n)
+            for(int i = 0; i < length; i++){
+                count.put(gene.charAt(i), count.get(gene.charAt(i)) + 1);
             }
 
-            if (isStable(count, stable)) {
+            if(isStable(count, stable)){
                 return 0;
-            } else {
-                windowL = 0;
-                int windowR = 0;
+            }
 
-                while(windowL < length && windowR < length) {
-                    if (!isStable(count, stable)) {
+            // O(n)
+            int windowL = 0;
+            int windowR = 0;
+            int min = length;
+
+            while(windowR < length && windowL < length){
+                for(;windowR < length;){
+                    if(!isStable(count, stable)){
                         count.put(gene.charAt(windowR), count.get(gene.charAt(windowR)) - 1);
-                        ++windowR;
-                    } else {
-                        min = Math.min(min, windowR - windowL);
-                        count.put(gene.charAt(windowL), count.get(gene.charAt(windowL)) + 1);
-                        ++windowL;
+                        windowR++;
+                        continue;
                     }
+
+                    break;
                 }
 
-                return min;
+                // decrease window size
+                for(; windowL < windowR;){
+                    if(isStable(count, stable)){
+                        min = Math.min(min, windowR - windowL);
+                        count.put(gene.charAt(windowL), count.get(gene.charAt(windowL)) + 1);
+                        windowL++;
+                        continue;
+                    }
+
+                    break;
+                }
             }
+
+            return min;
         }
 
         private static boolean isStable(HashMap<Character, Integer> map, int threshold) {
