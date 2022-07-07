523be63476dc973ce70cdfaf634097196d1e4110
donghoon-desktop <varensio3@gmail.com>
2022-07-07 18:57:58 +1000

    Longest Common Subsequence

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ import java.lang.reflect.Method;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        final String solutions = "Solutions";
-        final String problem = "SherlockAndAnagrams";
+        final String problem = "CommonChild";
+        final String name = String.join(".", new String[]{"ProblemSolving", "Algorithms", "Strings", problem});
         final String main = "main";
 
         try {
+            System.out.println(String.format("Solving problem: %s", name));
             System.out.println("Input from next line:");
-            Class<?> solution = Class.forName(String.format("%s.%s", solutions, problem));
+            Class<?> solution = Class.forName(name);
             Method method = solution.getMethod(main, String[].class);
             method.invoke(null, (Object) args);
         } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
