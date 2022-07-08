306bf05666a7757d639ae4065d17468a63672da4
donghoon-desktop <varensio3@gmail.com>
2022-07-09 04:39:19 +1000

    KnightLOnAChessboard

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        final String problem = "GridlandMetro";
+        final String problem = "KnightLOnAChessboard";
         final String name = String.join(".", new String[]{"ProblemSolving", "Algorithms", "Search", problem});
         final String main = "main";
 
