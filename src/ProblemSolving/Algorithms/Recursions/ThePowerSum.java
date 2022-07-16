4b700217d0f77c7beef898ea87893446f85f821b
donghoon-desktop <varensio3@gmail.com>
2022-07-17 02:57:35 +1000

    ThePowerSum

---
+++ src/ProblemSolving/Algorithms/Recursions/ThePowerSum.java
@@ -0,0 +1,69 @@
+package ProblemSolving.Algorithms.Recursions;
+
+import java.io.*;
+
+public class ThePowerSum {
+
+    class Result {
+
+        /*
+         * Complete the 'powerSum' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER X
+         *  2. INTEGER N
+         */
+
+        public static int powerSum(int X, int N) {
+
+            return powerSum(X, X, N, 1);
+        }
+
+        public static int powerSum(int X, int remaining, int N, int start) {
+            if (remaining == 0) {
+                return 1;
+            }
+
+            if (remaining == -1) {
+                return 0;
+            }
+
+            int count = 0;
+            int max = findNthRootFloor(remaining, N);
+
+            for (int i = start; i <= max; i++) {
+                count += powerSum(X, remaining - (int) Math.pow(i, N), N, i + 1);
+            }
+
+            return count;
+        }
+
+        private static int findNthRootFloor(int X, int N){
+            int i = 0;
+            while (Math.pow(i + 1, N) <= X){
+                i++;
+            }
+
+            return i;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.powerSum(X, N);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- src/SyntaxCheck.java
+++ src/SyntaxCheck.java
@@ -7,7 +7,7 @@ public class SyntaxCheck {
     public static void main(String[] args){
 
         for(int i = 1; i <= 100; i++){
-            System.out.println(String.format("%d:%d", i, countPrimeFactors(i)));
+            System.out.println(String.format("%d:%d", i, countOddPrimeFactors(i)));
         }
 
     }
@@ -18,6 +18,29 @@ public class SyntaxCheck {
         int count = 0;
         while (next != 1 && prime <= next){
             if(next % prime == 0){
+                do {
+                    count++;
+                    next = next / prime;
+                } while (next % prime == 0);
+            }
+
+            prime++;
+        }
+
+        return count;
+    }
+
+    private static int countOddPrimeFactors(int n) {
+        int next = n;
+        int prime = 2;
+        int count = 0;
+
+        while(next % 2 == 0){
+            next = next / 2;
+        }
+
+        while (next != 1 && prime <= next) {
+            if (next % prime == 0) {
                 do {
                     count++;
                     next = next / prime;
