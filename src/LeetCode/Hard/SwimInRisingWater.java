bd25a502c06044a5c8100056e46634f4cba6407d
donghoon-desktop <varensio3@gmail.com>
2022-07-25 21:19:10 +1000

    SwimInWater

--- src/LeetCode/Hard/SwimInRisingWater.java
+++ src/LeetCode/Hard/SwimInRisingWater.java
@@ -6,17 +6,10 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class SwimInRisingWater {
-    public class Node{
-        public int r;
-        public int c;
-        public int v;
-
-        public Node(int r, int c, int v){
-            this.r = r;
-            this.c = c;
-            this.v = v;
-        }
-    }
+    /*
+        Runtime: 24 ms, faster than 35.72% of Java online submissions for Swim in Rising Water.
+        Memory Usage: 42.5 MB, less than 85.83% of Java online submissions for Swim in Rising Water.
+     */
     public int swimInWater(int[][] grid) {
         int n = grid.length;
         int[][] directions = new int[4][2];
@@ -25,48 +18,46 @@ public class SwimInRisingWater {
         directions[2] = new int[]{0, 1};
         directions[3] = new int[]{0, -1};
 
-        int[][] record = new int[n][n];
-        for(int i = 0; i < n; i++){
-            Arrays.fill(record[i], Integer.MAX_VALUE);
-        }
-        record[0][0] = grid[0][0];
-
-        Queue<Node> heap = new PriorityQueue<>(10, Comparator.comparingInt(node -> node.v));
-        heap.add(new Node(0, 0, grid[0][0]));
+        boolean[][] visited = new boolean[n][n];
+        Queue<int[]> heap = new PriorityQueue<>(10, Comparator.comparingInt(node -> node[2]));
+        heap.add(new int[]{0, 0, grid[0][0]});
 
         while(!heap.isEmpty()){
-            Node curr = heap.poll();
+            int[] curr = heap.poll();
+            int currRow = curr[0];
+            int currColumn = curr[1];
+            int currValue = curr[2];
+
+            if(currRow == n - 1 && currColumn == n - 1){
+                return currValue;
+            }
+
+            visited[currRow][currColumn] = true;
 
             for(int[] direction: directions){
-                int nextR = curr.r + direction[0];
-                int nextC = curr.c + direction[1];
+                int nextR = currRow + direction[0];
+                int nextC = currColumn + direction[1];
 
                 if(nextR < 0 || nextR >= n || nextC < 0 || nextC >= n){
                     continue;
                 }
 
-                int nextValue = Math.max(curr.v, grid[nextR][nextC]);
-                if(nextValue < record[nextR][nextC]){
-                    record[nextR][nextC] = nextValue;
-                    heap.add(new Node(nextR, nextC, nextValue));
+                if(visited[nextR][nextC]){
+                    continue;
                 }
+
+                int nextValue = Math.max(currValue, grid[nextR][nextC]);
+                heap.add(new int[]{nextR, nextC, nextValue});
             }
         }
-
 
         return -1;
     }
 
-    private int extendedGet(int[][] arr, int r, int c){
-        if (r < 0 || c < 0 || r >= arr.length || c >= arr.length){
-            return Integer.MAX_VALUE;
-        }
-        return arr[r][c];
-    }
-
     public static void main(String[] args){
         SwimInRisingWater swim = new SwimInRisingWater();
-        int[][] grid = new int[][]{{0,1,2,3,4},{24,23,22,21,5},{12,13,14,15,16},{11,17,18,19,20},{10,9,8,7,6}};
+//        int[][] grid = new int[][]{{0,1,2,3,4},{24,23,22,21,5},{12,13,14,15,16},{11,17,18,19,20},{10,9,8,7,6}};
+        int[][] grid = new int[][]{{0,2},{1,3}};
         int result = swim.swimInWater(grid);
         System.out.println(result);
     }
