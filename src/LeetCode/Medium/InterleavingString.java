2b034ebd43e3a253b5173877fa53510fd7dc9818
donghoon-desktop <varensio3@gmail.com>
2022-07-31 23:48:36 +1000

    InterleavingString

--- src/LeetCode/Medium/InterleavingString.java
+++ src/LeetCode/Medium/InterleavingString.java
@@ -7,8 +7,8 @@ import java.util.Set;
 
 
 /*
-    Runtime: 4 ms, faster than 63.53% of Java online submissions for Interleaving String.
-    Memory Usage: 45.5 MB, less than 8.66% of Java online submissions for Interleaving String.
+    Runtime: 1 ms, faster than 99.11% of Java online submissions for Interleaving String.
+    Memory Usage: 40.1 MB, less than 99.81% of Java online submissions for Interleaving String.
  */
 public class InterleavingString {
 
@@ -18,24 +18,23 @@ public class InterleavingString {
 
         int l1 = s1.length();
         int l2 = s2.length();
-        int l3 = s3.length();
 
-        boolean[][][] memo = new boolean[l1 + 1][l2 + 1][l3 + 1];
+        boolean[][] memo = new boolean[l1 + 1][l2 + 1];
 
         inner(s1, s2, s3, 0, 0, 0, memo);
         return count > 0;
     }
 
-    private void inner(String s1, String s2, String s3, int i, int j, int k, boolean[][][] memo){
+    private void inner(String s1, String s2, String s3, int i, int j, int k, boolean[][] memo){
         if(count > 0){
             return;
         }
 
-        if(memo[i][j][k]){
+        if(memo[i][j]){
             return;
         }
 
-        memo[i][j][k] = true;
+        memo[i][j] = true;
 
         int l1 = s1.length();
         int l2 = s2.length();
@@ -49,12 +48,8 @@ public class InterleavingString {
             return;
         }
 
-        char ch1 = i < l1 ? s1.charAt(i) : '.';
-        char ch2 = j < l2 ? s2.charAt(j) : '.';
-        char ch3 = s3.charAt(k);
-
-        boolean s1Has = ch1 == ch3;
-        boolean s2Has = ch2 == ch3;
+        boolean s1Has = i < l1 && s1.charAt(i) == s3.charAt(k);
+        boolean s2Has = j < l2 && s2.charAt(j) == s3.charAt(k);
 
         if(s1Has && s2Has){
             inner(s1, s2, s3, i + 1, j, k + 1, memo);
