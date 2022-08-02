f197877a01a2c944be7fb26c800c117f8e8421f7
donghoon-desktop <varensio3@gmail.com>
2022-08-02 22:23:43 +1000

    LongestSubstringWithoutRepeatingCharacters

---
+++ src/LeetCode/Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,46 @@
+package LeetCode.Medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// start time: 10: 11
+// end time: 10:22
+
+/*
+    Runtime: 7 ms, faster than 82.89% of Java online submissions for Longest Substring Without Repeating Characters.
+    Memory Usage: 42.8 MB, less than 87.43% of Java online submissions for Longest Substring Without Repeating Characters.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0){
+            return 0;
+        }
+
+        int max = 1;
+        int i = 0, j = 1;
+        Set<Character> set = new HashSet<>();
+        set.add(s.charAt(i));
+         while(j < s.length()){
+            char ch = s.charAt(j);
+
+            if(set.contains(ch)){
+                set.remove(s.charAt(i++));
+
+            } else{
+                set.add(ch);
+                j++;
+            }
+
+            max = Math.max(max, j - i);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args){
+        String str = "pwwkew";
+        int result = (new LongestSubstringWithoutRepeatingCharacters()).lengthOfLongestSubstring(str);
+
+        System.out.println(result);
+    }
+}
